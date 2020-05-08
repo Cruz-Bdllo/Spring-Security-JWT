e857cd4413a27a60506792a8b2601e6d235abaf2
Cruz Badillo <cubj25@gmail.com>
2020-05-08 17:57:59 -0500

    run: Prueba para guardar un usuario creando la ruta en controlador y configuracion de seguridad para acceder a la ruta

--- src/main/java/com/auth/jwt/app/controller/HomeController.java
+++ src/main/java/com/auth/jwt/app/controller/HomeController.java
@@ -1,14 +1,43 @@
 package com.auth.jwt.app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.auth.jwt.app.entity.Role;
+import com.auth.jwt.app.entity.Usuario;
+import com.auth.jwt.app.repository.RoleRepository;
+import com.auth.jwt.app.service.IRoleService;
+import com.auth.jwt.app.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/public")
     public String homePublic(){
         return "Pagina de inicio al publico";
     } // fin de la peticion
+
+    // El formulario registrar contara con los campos: username, correo, password
+    @PostMapping("/registrarse")
+    public ResponseEntity<?> registrarse(@RequestBody Usuario usuario){
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+
+        // Asignar role de user
+        Role role = roleService.buscarRolePorId(3);
+        usuario.agregarRoleALista(role);
+        usuarioService.guardarUsuario(usuario);
+
+        return ResponseEntity.ok("Usuario registrado correctamente");
+    } // fin de la pagina de registro
 
 } // fin del controlador home
--- src/main/java/com/auth/jwt/app/security/SecurityConfigurer.java
+++ src/main/java/com/auth/jwt/app/security/SecurityConfigurer.java
@@ -1,23 +1,46 @@
 package com.auth.jwt.app.security;
 
+import com.auth.jwt.app.security.service.MiUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MiUserDetailsService userDetailsService;
 
+
+    @Bean
+    public BCryptPasswordEncoder passEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Indicamos que queremos una autenticacion personalizada en este caso definimos el comportamiento
+     * del <b>serDetailsService</b> en nuestra clase {@link MiUserDetailsService}, esto permite personalizar
+     * la autenticacion.
+     * Tambien indicamos que debe cifrar la contraseña que se cree o que se analice.
+     * @param auth usado para indicar la autenticacion por medio de la BD.
+     * @throws Exception si existe un problema con la autenticacion.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(userDetailsService).passwordEncoder(passEncoder());
     }
 
     // Establecemos que rutas y/o recursos estaran protegidos
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/*")
-                .permitAll();
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/registrarse")
+                    .permitAll()
+                    .antMatchers("/public")
+                    .permitAll();
     }
 } // fin de la clase de configuracion

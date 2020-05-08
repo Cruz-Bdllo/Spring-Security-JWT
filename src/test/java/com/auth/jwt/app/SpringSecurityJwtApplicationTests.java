29f176cefee96d6b8acc1f90f57b600ac8854786
Cruz Badillo <cubj25@gmail.com>
2020-05-08 13:09:53 -0500

    add: Creacion de la estructura de los paquetes.

---
+++ src/test/java/com/auth/jwt/app/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.jwt.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
